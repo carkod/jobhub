f156e0f7e839d21212e5067c185851d9a5911767
Carkod <carkodesign@gmail.com>
2017-11-23 01:33:54 +0000

    redirect after finished authentication request and adding state to avoid CRF attacks

--- back/Linkedin.js
+++ back/Linkedin.js
@@ -13,27 +13,26 @@ const IN = (app) => {
             new Error(error);
         }
         
-        handshake(req.query.code, res);
+        handshake(req.query.code, res, state);
         
     });
     
 }
 
-const handshake = (code, ores) => {
+const handshake = (code, ores, state) => {
   //set all required post parameters
     
     const config = {
         url: 'https://www.linkedin.com/oauth/v2/accessToken',
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
         params: {
             grant_type: "authorization_code",
             code: code,
             redirect_uri: 'http://cv-generator-carkod.c9users.io:8081/api/linkedin',//should match as in Linkedin application setup
             client_id: '78n5odk9nuiotg',
-            client_secret: '7Q1pIbXkNWbXMzWa'
+            client_secret: '7Q1pIbXkNWbXMzWa',
+            state: state,
         },
         paramsSerializer: function(params) {
             return querystring.stringify(params)
@@ -46,7 +45,7 @@ const handshake = (code, ores) => {
         console.log(resp);
     }).catch(error => {
         console.log('error in axios request');
-    });
+    }).then(() => ores.redirect('http://cv-generator-carkod.c9users.io:8080/jobs/linkedin'));
     
 }
 
