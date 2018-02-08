afbf2e34acf565954356720e36683507609e33b8
Carkod <carkodesign@gmail.com>
2018-02-08 00:13:32 +0000

    fix web pdf

--- web/src/actions/cats.js
+++ web/src/actions/cats.js
@@ -4,6 +4,16 @@ import axios from 'axios';
 
 export const SET_CATS = 'SET_CATS';
 
+function handleResponse(response) {
+    console.log(response)
+    if (response.ok) {
+        return response.json();
+    } else {
+        let error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+}
 
 export function setCats(cats) {
     return {
