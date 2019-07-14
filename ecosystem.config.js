8b4f7a6676b0b4479f5e133c13366fa0443e3d18
Carlos Wu <carkod@Carloss-MacBook-Pro.local>
2019-07-14 15:37:36 +0200

    correct back files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
       {
         name: "JobHub Back-end",
-        script: "back/dist/src/server.js",
+        script: "back/dist/server.js",
         //watch: true,
         env_production: {
             "PORT": 8081,
