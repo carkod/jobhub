93564991722dc49bcc419268b25bd6aed11700b2
Carlos Wu <carkod@Carloss-MacBook-Pro.local>
2019-07-14 15:32:52 +0200

    fix pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
       {
         name: "JobHub Back-end",
-        script: "back/dist/server.js",
+        script: "back/dist/src/server.js",
         //watch: true,
         env_production: {
             "PORT": 8081,
