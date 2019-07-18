40c7b7e5351a8ec414e805ad96e7cfa94ee452a6
Carlos Wu <carkod@Carloss-MacBook-Pro.local>
2019-07-19 00:48:46 +0200

    pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
       {
         name: "jobhub",
-        version: "0.9.3",
+        version: "0.9.5",
         script: "back/dist/server.js",
         //watch: true,
         env_production: {
@@ -14,7 +14,9 @@ module.exports = {
         name: "jobhub-webhook",
         version: "0.0.1",
         script: "webhook.js",
+        watch: true,
         env_production: {
+          "PORT": 8082,
           "NODE_ENV": "production"
         }
       }
