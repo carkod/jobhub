088869eae6a5e10b09d13fef969b338fba7cb532
Carlos Wu <carkod@Carloss-MacBook-Pro.local>
2019-07-19 00:55:02 +0200

    adding console

--- webhook.js
+++ webhook.js
@@ -10,7 +10,8 @@ http.createServer(function (req, res) {
     req.on('data', function(chunk) {
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
-        if (req.headers['x-hub-signature'] == secret) {
+        if (req.headers['x-hub-signature'] === sig) {
+            console.log('executing git commands in server...')
             exec('cd ' + repo + '&& git reset --hard && git pull origin master && npm run build && pm2 restart ' + appName);
         }
     });
