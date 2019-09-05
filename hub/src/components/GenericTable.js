0838775be5f1c6ec2c29ddf2d159909ad15ef40c
Carlos Wu <carkod@Carloss-MacBook-Pro.local>
2019-09-05 15:39:51 +0200

    comment table

--- hub/src/components/GenericTable.js
+++ hub/src/components/GenericTable.js
@@ -9,125 +9,125 @@ import { Dropdown, Pagination, Table } from 'semantic-ui-react';
 import { addNotification } from '../../actions/notification';
 
 class TrackingTable extends Component {
-	constructor(props) {
-		super(props);
-		this.stages = new AddNewApplicationConfig()
-		this.state = {
-			activeColumn: null,
-			direction: null,
-			activePage: 1,
-			activePageSize: 10,
-			totalPages: 1,
-			showArchive: false,
-			data: {
+	// constructor(props) {
+	// 	super(props);
+	// 	this.stages = new AddNewApplicationConfig()
+	// 	this.state = {
+	// 		activeColumn: null,
+	// 		direction: null,
+	// 		activePage: 1,
+	// 		activePageSize: 10,
+	// 		totalPages: 1,
+	// 		showArchive: false,
+	// 		data: {
 
-      },
-			pageddata: APPLIED_COMPANIES
-		}
-	}
+  //     },
+	// 		pageddata: APPLIED_COMPANIES
+	// 	}
+	// }
 
 
-	componentDidMount = () => {
+	// componentDidMount = () => {
 		
-	}
+	// }
 
-	componentWillReceiveProps = (nextProps) => {
+	// componentWillReceiveProps = (nextProps) => {
 		
-	}
+	// }
 
-	componentDidUpdate = (prevProps, prevState, snapshot) => {
-		const { data, totalPages, showArchive, activePage } = this.state 
-		if (prevState.data !== this.state.data) {
-			this.totalPages()
-			this.setState({
-				pageddata: this.paginatePages(data, activePage)
-			})
-		}
-		if (prevState.totalPages !== this.state.totalPages) {
-			this.setState({ pageddata: this.paginatePages(data, activePage) })
-		}
+	// componentDidUpdate = (prevProps, prevState, snapshot) => {
+	// 	const { data, totalPages, showArchive, activePage } = this.state 
+	// 	if (prevState.data !== this.state.data) {
+	// 		this.totalPages()
+	// 		this.setState({
+	// 			pageddata: this.paginatePages(data, activePage)
+	// 		})
+	// 	}
+	// 	if (prevState.totalPages !== this.state.totalPages) {
+	// 		this.setState({ pageddata: this.paginatePages(data, activePage) })
+	// 	}
 
-		if (prevState.showArchive !== this.state.showArchive) {
-			this.setState({ showArchive: showArchive})
-		}
+	// 	if (prevState.showArchive !== this.state.showArchive) {
+	// 		this.setState({ showArchive: showArchive})
+	// 	}
 
-	}
+	// }
 
-	openDetailPage(id) {
-		this.props.history.push(`/tracker/${id}`)
-	}
+	// openDetailPage(id) {
+	// 	this.props.history.push(`/tracker/${id}`)
+	// }
 
-	deleteApplication = id => {
-		this.props.deleteApplication(id).then(res => {
-			this.props.getdata()
-		})
-	}
+	// deleteApplication = id => {
+	// 	this.props.deleteApplication(id).then(res => {
+	// 		this.props.getdata()
+	// 	})
+	// }
 
-	moveNextStage = i => {
-		const { stages } = this.state.data[i];
-		const findStageIndex = stages.findIndex(x => !x.completed)
-		if (findStageIndex === -1) {
-			const action = {
-				type: 'NO_MORE_STAGES',
-			}
-			addNotification(action)
-			return false
-		}
-		const newData = update(this.state.data,
-			{
-				[i]: { stages: { [findStageIndex]: { completed: { $set: true } } } }
-			}
-		)
-		this.setState({ data: newData })
-		this.props.moveNextStage(this.state.data)
-	}
+	// moveNextStage = i => {
+	// 	const { stages } = this.state.data[i];
+	// 	const findStageIndex = stages.findIndex(x => !x.completed)
+	// 	if (findStageIndex === -1) {
+	// 		const action = {
+	// 			type: 'NO_MORE_STAGES',
+	// 		}
+	// 		addNotification(action)
+	// 		return false
+	// 	}
+	// 	const newData = update(this.state.data,
+	// 		{
+	// 			[i]: { stages: { [findStageIndex]: { completed: { $set: true } } } }
+	// 		}
+	// 	)
+	// 	this.setState({ data: newData })
+	// 	this.props.moveNextStage(this.state.data)
+	// }
 
-	handleSort = clickedColumn => () => {
-		const { activeColumn, data, direction } = this.state
+	// handleSort = clickedColumn => () => {
+	// 	const { activeColumn, data, direction } = this.state
 
-		if (activeColumn !== clickedColumn) {
-			this.setState({
-				activeColumn: clickedColumn,
-				data: _.sortBy(data, [clickedColumn]),
-				direction: 'ascending',
-			})
+	// 	if (activeColumn !== clickedColumn) {
+	// 		this.setState({
+	// 			activeColumn: clickedColumn,
+	// 			data: _.sortBy(data, [clickedColumn]),
+	// 			direction: 'ascending',
+	// 		})
 
-			return
-		}
+	// 		return
+	// 	}
 
-		this.setState({
-			data: data.reverse(),
-			direction: direction === 'ascending' ? 'descending' : 'ascending',
-		})
-	}
+	// 	this.setState({
+	// 		data: data.reverse(),
+	// 		direction: direction === 'ascending' ? 'descending' : 'ascending',
+	// 	})
+	// }
 
-	handlePaginationChange = (e, p) => {
-		this.setState({ 
-      activePage: p.activePage, 
-      pageddata: this.paginatePages(this.state.data, p.activePage) 
-    })
-	}
+	// handlePaginationChange = (e, p) => {
+	// 	this.setState({ 
+  //     activePage: p.activePage, 
+  //     pageddata: this.paginatePages(this.state.data, p.activePage) 
+  //   })
+	// }
 
-	totalPages() {
-		const { data, activePageSize } = this.state
-		if (data.length < activePageSize) {
-			this.setState({ totalPages: 1 })
-			return 1
-		} else {
-			// Is there a remainder?
-			const remainder = (data.length % activePageSize) === 0
-			const calculatePages = data.length / activePageSize
-			const result = remainder ? calculatePages : Math.floor(calculatePages) + 1
-			this.setState({ totalPages: result })
-			return result
-		}
-	}
+	// totalPages() {
+	// 	const { data, activePageSize } = this.state
+	// 	if (data.length < activePageSize) {
+	// 		this.setState({ totalPages: 1 })
+	// 		return 1
+	// 	} else {
+	// 		// Is there a remainder?
+	// 		const remainder = (data.length % activePageSize) === 0
+	// 		const calculatePages = data.length / activePageSize
+	// 		const result = remainder ? calculatePages : Math.floor(calculatePages) + 1
+	// 		this.setState({ totalPages: result })
+	// 		return result
+	// 	}
+	// }
 
-	paginatePages(data, activePage) {
-		const { activePageSize } = this.state
-		const pageddata = data.slice((activePage - 1) * activePageSize, activePageSize * activePage);
-		return pageddata
-	}
+	// paginatePages(data, activePage) {
+	// 	const { activePageSize } = this.state
+	// 	const pageddata = data.slice((activePage - 1) * activePageSize, activePageSize * activePage);
+	// 	return pageddata
+	// }
 
 	render() {
 		const { data, activeColumn, direction, activePage, activePageSize, totalPages, pageddata } = this.state
@@ -138,7 +138,7 @@ class TrackingTable extends Component {
 				<Table.Header>
 					<Table.Row>
 						{columns.map((col, i) =>
-							<Table.HeaderCell key={i} sorted={activeColumn === col ? direction : null} onClick={this.handleSort(col)}>
+							<Table.HeaderCell key={i} sorted={activeColumn === col ? direction : null}>
 								{col}
 							</Table.HeaderCell>
 						)}
