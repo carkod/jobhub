fd2c377db6f8841e0297370100fb3f6c0b51ee3b
Carlos Wu Fei <carkod@Carloss-MacBook-Pro.local>
2023-02-02 21:30:14 +0000

    Standardize styling variables and spacing

--- web/src/pages/blog/BlogIndex.js
+++ web/src/pages/blog/BlogIndex.js
@@ -28,22 +28,24 @@ export default class BlogIndex extends Component {
           <link rel="canonical" href={process.env.REACT_APP_HOME_URL + "/blog"} />
         </Helmet>
         <main className="container">
-          <div>
+          <div style={{marginBottom: "2rem"}}>
             <h1>{`B-Log`}</h1>
             <p>The server log of my life, a place where I jot down incoherent notes for preservation in eternity.</p>
           </div>
+          <div className="u-block__wrapper">
           {this.state.blogs.length > 0
             ? this.state.blogs.map((b) => (
-                <div key={b._id} className="row one column wide">
-                  <section id="blogs" class="u-blog-header-section ">
+                <div key={b._id} className="u-block__item">
+                  <section id="blogs" class="u-blog-header-section">
                     <h3 className="u-blog-index-header">
                       <a href={`/blog/${b._id}`}>{b.name}</a>
                     </h3>
-                    <small>Category: {b.category}</small>
                   </section>
+                  <small>Category: {b.category}</small>
                 </div>
               ))
             : "No logs yet"}
+            </div>
         </main>
       </Fragment>
     );
